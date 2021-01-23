26794676ed070b8f390d32cfde5394fa3c182b82
YiroYi <yiroyi@live.com>
2021-01-23 01:02:46 -0600

    Adding first test for the main page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test("renders button", () => {
+
+});
+
+test("renders counter display", () => {
+
+});
+
+test("counter starts at 0", () => {
+
+});
+
+test("clicking on button increments counter dipslay", () => {
+
+});
+
+/*TDD es primero hacer los test de lo que quieres que
+haga tu app , por ejemplo podrias ser el pseudo code
+y de ahi comenza con el ejemplo de arriba basicamente
+escribiste el pseudo code de lo que quieres que la
+interfase haga*/
